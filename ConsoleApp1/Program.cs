73b0ea2280f0e0bd2f803f77e83cca8522f4e434
rohliksesyrem <jiri.rzyman@infis.cz>
2026-04-30 10:31:51 +0200

    aaaaaa

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("DEJ mi čislo");
+var a =  Int32.Parse(Console.ReadLine());
+Console.WriteLine("Dej mi druhé číslo");
+var b = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Co chces to sečíst, vydělit, vynásobit nebo odečíst");
+
+string znam = Console.ReadLine();
+
+ 
+if (znam == "+") 
+{
+    Console.WriteLine(a + b);
+}
+
+ else if (znam == "-") ;
+{
+    Console.WriteLine(a - b);
+}
+ else if (znam == "*") ;
+{
+    Console.WriteLine(a * b);
+}
+  else if (znam == "/") ;
+{
+    Console.WriteLine(a / b);
+} 
